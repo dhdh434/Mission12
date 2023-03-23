958909d20e1daf605c427a735e57ed86c1930eca
David <davidshunter2@gmail.com>
2023-03-23 01:39:41 -0600

    fg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './CollegeBasketballTeams.json';
+
+
+
+class BballTeam extends React.Component{
+  render() {
+
+    const oneTeam = data.teams[1];
+
+    return(
+      <div>
+
+        
+
+        <h2>Name: {oneTeam.school}</h2>
+        <h3>Mascot Name: {oneTeam.name}</h3>
+        <h3>Location: {oneTeam.city}, {oneTeam.state}</h3>
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is a march madness website</h1>
+        <h3>Basketball Teams:</h3>
 
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {data.teams.map(oneTeam => <BballTeam {...oneTeam}/>)}
         <a
           className="App-link"
           href="https://reactjs.org"
